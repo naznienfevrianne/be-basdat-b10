62881314155384c10d4f6416ed53fa95961c439d
Naznien Fevrianne Malano <111237114+naznienfevrianne@users.noreply.github.com>
2023-08-06 08:00:52 +0700

    Update settings.py

--- project_django/settings.py
+++ project_django/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = 'django-insecure-3@5wx%(^*zl68l(o$^m-3%cvjv0g&mom1ra=oj5f048_al57s7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['basdat-b10.up.railway.app','127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://basdat-b10.up.railway.app']
+ALLOWED_HOSTS = ['basdat-b10.vercel.app','127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://basdat-b10.vercel.app']
 # Application definition
 
 INSTALLED_APPS = [
